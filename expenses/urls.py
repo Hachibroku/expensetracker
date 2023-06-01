4949a90743394280282ce815499ebcc24ba8be39
Murphey Osmundson <mosmundson88@gmail.com>
2023-06-01 15:47:17 -0400

    Feature 7 complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from receipts.views import show_receipt, redirect_home
 
 urlpatterns = [
     path("", redirect_home, name="redirect_home"),
-    path("receipts/", show_receipt, name="home"),
+    path("receipts/", include("receipts.urls")),
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
 ]
